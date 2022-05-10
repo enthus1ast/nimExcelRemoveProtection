82f7db90044e9468c61ad15f3a5493e39029dc7a
David Krause <enthus1ast@users.noreply.github.com>
2022-05-10 11:35:01 +0200

    init

---
+++ nimExcelRemoveProtection.nimble
@@ -0,0 +1,14 @@
+# Package
+
+version       = "0.1.0"
+author        = "David Krause"
+description   = "Remove excel write protection"
+license       = "MIT"
+srcDir        = "src"
+
+
+# Dependencies
+
+requires "nim >= 1.6.4"
+requires "zippy"
+requires "cligen"
---
+++ src/nimExcelRemoveProtection.nim
@@ -0,0 +1,51 @@
+import zippy, cligen
+import strutils, os
+import std/prelude
+import zippy/ziparchives
+# import print
+
+iterator allSheets(): string =
+  for path in walkPattern(getAppDir() / "unzipped" / "xl" / "worksheets" / "sheet*.xml"):
+    yield path
+
+proc breakProtection(sheet, data: string): string =
+  const starter = "<sheetProtection"
+  const ender = "/>"
+  result = data
+  var pos = data.find starter
+  if pos > -1:
+    var endPos = data.find(ender, pos + starter.len)
+    if endPos == -1:
+      echo "Ender not found... error"
+      return
+    endPos.inc ender.len
+    # print sheet, "protected", pos, endPos
+    result.delete(pos..endPos - 1)
+
+
+proc remove(path: string) =
+  if not path.endswith(".xlsx"):
+    echo "not an excel file (.xlsx missing)"
+    return
+  if not path.fileExists():
+    echo "File does not exists: ", path
+    return
+  try:
+    extractAll(path, getAppDir() / "unzipped")
+  except:
+    discard
+  for sheet in allSheets():
+    var data = readFile(sheet)
+    let dataBroken = breakProtection(sheet, data)
+    writeFile(sheet, dataBroken)
+  let fileParts = splitFile(path)
+  let newPath = fileParts.dir / fileParts.name & "_broken" & fileParts.ext
+  let unzipPath = getAppDir() / "unzipped/"
+  createZipArchive(unzipPath, newPath)
+  echo "Written to: ", newPath
+  removeDir(unzipPath)
+
+when isMainModule:
+  # remove("""c:\Users\david\AppData\Local\Temp\martinSchr\2021_MembersIBC2.xlsx""")
+  dispatch(remove)
+
